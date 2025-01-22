f98800300d4732bcc0f85bc298f10c8db9a7bc7b
hellonearth311 <narangswarit02@gmail.com>
2025-01-21 21:20:15 -0600

    Initial commit

---
+++ src/main/java/net/hellonearth311/powderman/PowdermanModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.hellonearth311.powderman;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PowdermanModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
